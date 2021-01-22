4e4c5bb624ef4822aed418b273ca8a3e2ee1a390
Sarah Finck <sarahfinck95@gmail.com>
2021-01-21 22:42:17 -0600

    created weather API url

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // search for a city 
 
-// pull current and future conditions 
+var weather = '';
+var api = 'api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&appid=3fbaec1ab0dd5f5d8247be45d836ab90';
+var units = '&units=imperial';
+var input = '';
+
+function setup() {
+    var button = $('#search');
+    button.onclick(cityInput);
+    
+    input = $('#city')
+}
+function cityInput(){
+    var url = api + input.value() + apiKey + units;
+    loadJSON(url, gotData);
+}
+function gotData(data) {
+    weather = data;
+}
 
 // add search to local 
 
-// Current weather condiiton is displayed in current city card 
+// Current weather condition is displayed in current city card
+
 // Name, date, icon rep of weather conditions (temp, humidity, wind speed, UV index
 
 // uv index - changes color indicating favorable, moderate or severe
@@ -17,3 +36,4 @@
 
 // when weather dashboard is open 
 // presented with the last city searched 
+
