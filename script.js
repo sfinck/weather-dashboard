10b48e308f1dec9e18f8c3e8c4f195441809c898
Sarah Finck <sarahfinck95@gmail.com>
2021-01-21 21:13:03 -0600

    created index.html layout, pseudo-coded js file

---
+++ script.js
@@ -0,0 +1,19 @@
+// search for a city 
+
+// pull current and future conditions 
+
+// add search to local 
+
+// Current weather condiiton is displayed in current city card 
+// Name, date, icon rep of weather conditions (temp, humidity, wind speed, UV index
+
+// uv index - changes color indicating favorable, moderate or severe
+
+// view future weather conditions
+// 5-day forecast date, icon weather representation of coniitons, temp and humidity 
+
+// click on a city on the left in the search history 
+// then i am presented with the conditions for that city again 
+
+// when weather dashboard is open 
+// presented with the last city searched 
